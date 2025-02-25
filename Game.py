015a8ac702f25dfea6e30c729d1fe70562feaba3
DanielBramlett <daniel.bramlett@Timothyministry.org>
2025-02-25 12:52:26 +0000

    Finshed

--- Game.py
+++ Game.py
@@ -1,29 +1,39 @@
 import pyglet
 from pyglet.shapes import Rectangle, Circle
 from pyglet.window import key
+from random import randint
 
-#Window config
+# windows config
 window_width = 800
 window_height = 600
 window = pyglet.window.Window(width=window_width, height=window_height, caption="My Game")
 
-#Color constant
-GRAY = (125,125,125)
-GREEN = (34,170,34)
-BLACK = (0,0,0)
+#color constants
+GRAY = (169,169,169)
+GREEN = (34,139,34)
+WHITE = (255,255,255)
+RED = (255,0,0)
 
-#Player config
+#player config
 player_radius = 40
 player_x = window_width / 2
 player_y = 100
 player_speed = 150
 
-#Handles key input
+# This handles keyboard input
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
+player = Circle(player_x, player_y, player_radius, color=WHITE)
 
-player = Circle(player_x, player_y, player_radius, color=BLACK)
+obstacle_size = 60
+obstacle_speed = 200
+obstacles = []
+
+def add_obstacle(dt):
+    x = randint(int(path_x), int(path_x + path_width - obstacle_size)) 
+    obstacle = Rectangle(x, window_height-obstacle_size,obstacle_size,obstacle_size,RED)
+    obstacles.append(obstacle)
 
 path_width = 400
 path_x = (window_width - path_width)/2
@@ -31,30 +41,26 @@ path_x = (window_width - path_width)/2
 background = Rectangle(0,0,window_width, window_height, GREEN )
 
 path = Rectangle(path_x,0,path_width,window_height,GRAY)
-dir = 'right'
+
 def update(dt):
-
-    global dir
-
+    #Moves player
     if keys[key.LEFT] and player.x > path_x+player_radius:
         player.x -= player_speed * dt
     if keys[key.RIGHT] and player.x < path_width+path_x-player_radius:
         player.x += player_speed *dt
     if keys[key.UP]and player.y < window_height:
        player.y += player_speed *dt
-    if keys[key.DOWN]and player.y > 0:
+    if keys[key.DOWN]and player.y > player_radius:
        player.y -= player_speed *dt
+    
+    for obstacle in obstacles[:]:
+        obstacle.y -= obstacle_speed * dt
 
-    #Moves player left and right
-    # if dir == 'right':
-    #     player.x = player.x + player_speed * dt
-    # elif dir == 'left':
-    #     player.x = player.x - player_speed * dt
+        if obstacle.y + obstacle_size < 0:
+            obstacle.delete()
+            obstacles.remove(obstacle)
 
-    # if player.x > path_width+path_x-player_radius and dir == 'right':
-    #     dir = 'left'
-    # elif player.x < path_x+player_radius and dir =='left':
-    #     dir = 'right'
+
 
 @window.event
 def on_draw():
@@ -62,6 +68,9 @@ def on_draw():
     background.draw() 
     path.draw()  
     player.draw()
+    for obstacle in obstacles[:]:
+        obstacle.draw()
 
+pyglet.clock.schedule_interval(add_obstacle, 2)
 pyglet.clock.schedule_interval(update, 1/60.0)
 pyglet.app.run()
